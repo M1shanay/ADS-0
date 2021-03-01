1341ce8dbc315fef53b3af215bb2fece565a335d
M1shanay <78785594+M1shanay@users.noreply.github.com>
2021-03-01 12:10:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    while ( a!= b) {
-       if (a > b)
-            a -= b;
-        if (a < b)
-            b -= a;
+ while (a != b) {
+        if (a > b){
+            a -= b;}
+        if (a < b){
+            b -= a;}
     }
     return a;
 }
