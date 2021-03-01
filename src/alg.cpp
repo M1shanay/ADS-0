5bdcdbac147cc219219964a6a9015568d2135708
M1shanay <78785594+M1shanay@users.noreply.github.com>
2021-03-01 12:16:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,10 +2,10 @@
 
 int gcd(int a, int b) {
  while (a != b) {
-        if (a > b){
-            a -= b;}
-        if (a < b){
-            b -= a;}
+        if (a > b)
+            a -= b;
+        else
+            b -= a;
     }
     return a;
 }
